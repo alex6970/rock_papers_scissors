9a3953303a4203cd567d9f34bfad87cec58098be
Goncalves <alexandra.goncalves@dxc.com>
2022-11-15 17:10:10 +0100

    Fixed simple rand function

--- javascript.js
+++ javascript.js
@@ -1,1 +1,17 @@
-console.log("Hello World !")
+let hands = ['Rock', 'Papers', 'Scisors']
+
+
+function getComputerChoice() {
+    let randHands = hands[Math.floor(Math.random() * hands.length)]
+    return randHands
+}
+
+console.log(getComputerChoice())
+
+// function playRound(playerSelection, computerSelection) {
+//     // your code here!
+// }
+
+// const playerSelection = "rock";
+// const computerSelection = getComputerChoice();
+// console.log(playRound(playerSelection, computerSelection));
