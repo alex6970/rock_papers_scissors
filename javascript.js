fd58595b8c8676d4ad30a1861500ce166f16a676
Goncalves <alexandra.goncalves@dxc.com>
2022-11-16 12:00:43 +0100

    Fixing while loop

--- javascript.js
+++ javascript.js
@@ -1,4 +1,4 @@
-let hands = ['Rock', 'Papers', 'Scisors']
+const hands = ['Rock', 'Papers', 'Scisors']
 
 
 function getComputerChoice() {
@@ -6,12 +6,60 @@ function getComputerChoice() {
     return randHands
 }
 
-console.log(getComputerChoice())
+function getPalyerSelection(nb) {
+    if (nb == 1) {
+        return "Rock"
 
-// function playRound(playerSelection, computerSelection) {
-//     // your code here!
-// }
+    } else if (nb == 2) {
+        return "Papers"
 
-// const playerSelection = "rock";
-// const computerSelection = getComputerChoice();
-// console.log(playRound(playerSelection, computerSelection));
+    } else {
+        return "Scisors"
+
+    }
+}
+
+function playRound(playerSelection, computerSelection) {
+    if ((playerSelection === "Rock" && computerSelection == "Rock") || (playerSelection === "Papers" && computerSelection == "Papers") || (playerSelection === "Scisors" && computerSelection == "Scisors")) {
+        return ("Draw ! "  + playerSelection + " equals " + computerSelection)
+
+    } else if ((playerSelection === "Papers" && computerSelection == "Rock") || (playerSelection === "Scisors" && computerSelection == "Papers") || (playerSelection === "Rock" && computerSelection == "Scisors")) {
+        return ("You win ! " + playerSelection + " beats " + computerSelection)
+
+    } else {
+        return ("You lost :(, " + computerSelection + " beats " + playerSelection)
+    }
+}
+
+
+let playerSelectionPrompt // = undefined
+let playerSelection // = undefined
+
+let computerSelection // = undefined
+
+const choices = ["1", "2", "3", "Exit"]
+
+
+
+while (playerSelectionPrompt != "Exit") {
+    
+    playerSelectionPrompt = prompt("Please submit your choice : \n 1 : Rock \n 2 : Papers \n 3 : Scisors \n\n Submit Exit to quit")
+
+    while (!playerSelectionPrompt ) {
+        alert("Wrong input ! You must submit a number between 1 and 3 !!")
+        playerSelectionPrompt = prompt("Please submit your choice : \n 1 : Rock \n 2 : Papers \n 3 : Scisors")
+    }
+
+    playerSelection = getPalyerSelection(playerSelectionPrompt)
+    computerSelection = getComputerChoice()
+
+
+    alert("Computer's choice was : ", computerSelection)
+    alert(playRound(playerSelection, computerSelection))
+
+}
+
+alert("Bye Bye !")
+
+
+
